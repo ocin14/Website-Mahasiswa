74d841367220222b7fe372121164e91408c392cc
ocin14 <new.nikolee2007@gmail.com>
2024-02-21 20:31:11 +0700

    edit

--- WebCABDIN/src/App.vue
+++ WebCABDIN/src/App.vue
@@ -5,10 +5,10 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/img/logo-provinsi-jawa-timur-KECIL.png" width="250" height="250" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Welcome to Mahasiswa Website!!" />
     </div>
   </header>
 
